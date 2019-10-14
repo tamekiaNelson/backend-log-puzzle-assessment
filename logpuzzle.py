980df3a152428ca56a64b535b470112a2561e13b
Tamekia N <tamekialashay@outlook.com>
2019-10-13 20:13:17 -0400

    completed turning in final version

--- logpuzzle.py
+++ logpuzzle.py
@@ -31,8 +31,20 @@ def read_urls(filename):
     extracting the hostname from the filename itself.
     Screens out duplicate urls and returns the urls sorted into
     increasing order."""
-    # +++your code here+++
-    pass
+
+    puzzle = []
+    urls = []
+    with open(filename) as f:
+        for line in f:
+            url = re.findall(r'(\S*puzzle\S*.jpg)', line)
+            if url and url[0] not in puzzle:
+                puzzle.append(url[0])
+        puzzle.sort(
+            key=lambda x: re.search(r'\w[^-]*$', x).group(0))
+        for item in puzzle:
+            img = 'http://code.google.com{}'.format(item)
+            urls.append(img)
+    return urls
 
 
 def download_images(img_urls, dest_dir):
@@ -43,16 +55,25 @@ def download_images(img_urls, dest_dir):
     with an img tag to show each local image file.
     Creates the directory if necessary.
     """
-    # +++your code here+++
-    pass
+    if not os.path.exists(str(dest_dir)):
+        os.mkdir(dest_dir)
+    print('Retreiving...')
+    with open(str(dest_dir) + "/index.html", 'w') as file:
+        file.write("<html><body>")
+        for index, url in enumerate(img_urls):
+            urllib.urlretrieve(url, filename=dest_dir +
+                               '/img' + str(index) + ".jpg")
+            file.write("<img src='img" + str(index) + ".jpg' >")
+        file.write("</body></html>")
 
 
 def create_parser():
     """Create an argument parser object"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--todir',
-                        help='destination directory for downloaded images')
-    parser.add_argument('logfile', help='apache logfile to extract urls from')
+    parser.add_argument(
+        '-d', '--todir', help='destination directory for downloaded images')
+    parser.add_argument(
+        'logfile', help='apache logfile to extract urls from')
 
     return parser
 
@@ -64,11 +85,8 @@ def main(args):
     if not args:
         parser.print_usage()
         sys.exit(1)
-
     parsed_args = parser.parse_args(args)
-
     img_urls = read_urls(parsed_args.logfile)
-
     if parsed_args.todir:
         download_images(img_urls, parsed_args.todir)
     else:
